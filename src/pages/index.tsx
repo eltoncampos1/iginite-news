37d958001214c3e2911975dcd9824c01d4e24336
eltoncampos1 <eltoncampos36@gmail.com>
2021-07-23 15:21:16 -0300

    chore: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from './home.module.scss'
+
+import AvatarSvg from '../../public/images/avatar.svg'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>hello</h1>
+      
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News aboute the  <span>React</span> World</h1>
+          <p>
+            Get acess to all the publications <br></br>
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <Image src={AvatarSvg} alt="Girl coding" />
+      </main>
     </>
   )
 }
