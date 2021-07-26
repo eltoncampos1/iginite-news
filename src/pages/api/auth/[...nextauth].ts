87b900e8441cd17001bab516a40fa34376e22b83
eltoncampos1 <eltoncampos36@gmail.com>
2021-07-26 17:41:27 -0300

    chore: handling errorsduplicate users in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,24 +13,38 @@ export default NextAuth({
             scope:'read:users'
         }),
     ],
-    jwt: {
-        signingKey: process.env.JWT_SIGNIN_KEY
-    },
     callbacks: {
         async signIn(user, acc, profile) {
             const { email } = user;
 
             try {
                 await fauna.query (
-                    q.Create(
-                        q.Collection('users'),
-                        { data: { email }}
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email }}
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
                     )
                 )
                 return true
-            } catch {
-                return false
+            } catch(err) {
+                console.error(err.message);
                 
+                return false
             }
 
         }
